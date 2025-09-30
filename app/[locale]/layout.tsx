d1ce0fa6e0c85a728d575945c9f2c4ba02d96636
crucibleos <norris@servicecrucible.com>
2025-09-30 10:34:30 -0700

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,10 +12,10 @@ import { ReactNode } from "react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
-const APP_NAME = "Chatbot UI"
-const APP_DEFAULT_TITLE = "Chatbot UI"
-const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
-const APP_DESCRIPTION = "Chabot UI PWA!"
+const APP_NAME = "Crucible OS"
+const APP_DEFAULT_TITLE = "Crucible OS"
+const APP_TITLE_TEMPLATE = "%s - Crucible OS"
+const APP_DESCRIPTION = "Crucible OS AI"
 
 interface RootLayoutProps {
   children: ReactNode
